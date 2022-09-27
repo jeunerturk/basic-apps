59d4773118f278667471e67ea04726816a006aed
Umut Erturk <umutc.erturk@gmail.com>
2022-09-28 02:20:56 +0300

    Add Typing Animation To Welcome Screen

--- Angela Yu's iOS Bootcamp/FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Angela Yu's iOS Bootcamp/FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -6,7 +6,16 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { [self] Timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
        
     }
     
