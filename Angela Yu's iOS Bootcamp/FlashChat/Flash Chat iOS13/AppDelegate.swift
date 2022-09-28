d268b8307b17e508065fcfa8de0206f5e58c97c7
Umut Erturk <umutc.erturk@gmail.com>
2022-09-28 17:22:02 +0300

    Add Firebase To The Project Using CocoaPods
    Perform changes to get rid of error messages.

--- Angela Yu's iOS Bootcamp/FlashChat/Flash Chat iOS13/AppDelegate.swift
+++ Angela Yu's iOS Bootcamp/FlashChat/Flash Chat iOS13/AppDelegate.swift
@@ -1,4 +1,7 @@
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
--- Angela Yu's iOS Bootcamp/FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Angela Yu's iOS Bootcamp/FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,22 +1,14 @@
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { [self] Timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
-       
+        titleLabel.text = "⚡️FlashChat"
     }
     
 
